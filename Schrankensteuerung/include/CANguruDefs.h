1f92608df667e591055e82cb767130273929c6c6
CANguru <DiMo-CANguru@web.de>
2019-11-08 16:26:47 +0100

    Add files via upload

--- Schrankensteuerung/include/CANguruDefs.h
+++ Schrankensteuerung/include/CANguruDefs.h
@@ -12,20 +12,19 @@
 #define CANguruDefs
 
 // allgemein
-#define wait_time_long	  500
-#define wait_time_medium	50
-#define wait_time_small	  10
-#define wait_time   		  125
-#define max_char		      50
-#define min_char	    	  2
-#define min_char1	    	  3
-#define bdrMonitor	      115200
-
+#define wait_time_long 500
+#define wait_time_medium 50
+#define wait_time_small 10
+#define wait_time 125
+#define max_char 50
+#define min_char 2
+#define min_char1 3
+#define bdrMonitor 115200
 
 /*
  *  Gerätetypen
  */
-  /*
+/*
   Prio	2+2 Bit Message Prio			28 .. 25
 										XXXX
   Priorität (Prio)
@@ -43,36 +42,36 @@
   Rest Frei								1111
 */
 
-#define UID_BASE  0x45009195ULL    //CAN-UID
+#define UID_BASE 0x45009195ULL //CAN-UID
 #define maxdevice 99
 
-#define DEVTYPE_GFP       0x0000
-#define DEVTYPE_GB        0x0010
-#define DEVTYPE_CONNECT   0x0020
-#define DEVTYPE_MS2       0x0030
-#define DEVTYPE_WDEV      0x00E0
-#define DEVTYPE_CS2       0x00FF
-#define DEVTYPE_FirstCANguru  0x004F
-#define DEVTYPE_BASE      0x0050
-#define DEVTYPE_SERVO     0x0053
-#define DEVTYPE_RM        0x0054
-#define DEVTYPE_LIGHT     0x0055
-#define DEVTYPE_SIGNAL    0x0056
+#define DEVTYPE_GFP 0x0000
+#define DEVTYPE_GB 0x0010
+#define DEVTYPE_CONNECT 0x0020
+#define DEVTYPE_MS2 0x0030
+#define DEVTYPE_WDEV 0x00E0
+#define DEVTYPE_CS2 0x00FF
+#define DEVTYPE_FirstCANguru 0x004F
+#define DEVTYPE_BASE 0x0050
+#define DEVTYPE_SERVO 0x0053
+#define DEVTYPE_RM 0x0054
+#define DEVTYPE_LIGHT 0x0055
+#define DEVTYPE_SIGNAL 0x0056
 #define DEVTYPE_LEDSIGNAL 0x0057
-#define DEVTYPE_CANFUSE   0x0058
-#define DEVTYPE_GATE      0x0059
-#define DEVTYPE_LastCANguru  0x005F
+#define DEVTYPE_CANFUSE 0x0058
+#define DEVTYPE_GATE 0x0059
+#define DEVTYPE_LastCANguru 0x005F
 
-#define BASE_Offset      0x01
-#define DECODER_Offset   0x02
+#define BASE_Offset 0x01
+#define DECODER_Offset 0x02
 
 /*
  * Adressbereiche:
 */
-#define MM_ACC 		0x3000	  //Magnetartikel Motorola
-#define DCC_ACC 	0x3800	  //Magbetartikel NRMA_DCC
-#define MM_TRACK 	0x0000	  //Gleissignal Motorola
-#define DCC_TRACK 	0xC000    //Gleissignal NRMA_DCC
+#define MM_ACC 0x3000    //Magnetartikel Motorola
+#define DCC_ACC 0x3800   //Magbetartikel NRMA_DCC
+#define MM_TRACK 0x0000  //Gleissignal Motorola
+#define DCC_TRACK 0xC000 //Gleissignal NRMA_DCC
 
 /*
  * CAN-Befehle (Märklin)
@@ -96,28 +95,28 @@
 #define Bootloader_Track    0x1C
 #define Data_Query          0x20
 #define Config_Data_Stream  0x21*/
-#define SYS_CMD		          0x00 	//Systembefehle
-#define SYS_STAT	          0x0B	//System - Status (sendet geänderte Konfiguration)
-#define SWITCH_ACC 	        0x16	//Magnetartikel schalten
-#define S88_Polling         0x20
-#define S88_EVENT	          0x22	//Rückmelde-Event
-#define PING 		            0x30	//CAN-Teilnehmer anpingen
-#define CONFIG_Status       0x3A
+#define SYS_CMD 0x00    //Systembefehle
+#define SYS_STAT 0x0B   //System - Status (sendet geänderte Konfiguration)
+#define SWITCH_ACC 0x16 //Magnetartikel schalten
+#define S88_Polling 0x20
+#define S88_EVENT 0x22 //Rückmelde-Event
+#define PING 0x30      //CAN-Teilnehmer anpingen
+#define CONFIG_Status 0x3A
 
-#define stopp               0x00
-#define halt                0x02
-#define overload            0x0A
+#define stopp 0x00
+#define halt 0x02
+#define overload 0x0A
 
 /*
  * CAN-Befehle (eigene)
 */
-#define ConfigData        0x40
-#define ConfigData_R      0x41
-#define MfxProc           0x50	
-#define MfxProc_R         0x51	
-#define sendInitialData   0x52	
-#define BlinkAlive        0x60
-#define S88_EVENT_R	      S88_EVENT+1	// Rück-Rückmelde-Event
+#define ConfigData 0x40
+#define ConfigData_R 0x41
+#define MfxProc 0x50
+#define MfxProc_R 0x51
+#define sendInitialData 0x52
+#define BlinkAlive 0x60
+#define S88_EVENT_R S88_EVENT + 1 // Rück-Rückmelde-Event
 
 // damit 1024 (eigentlich 1023) Artikel adressiert werden können,
 // bedarf es 1024/4 Decoder; das 256 bzw. bei 255 entspr. 0xFF
--- Schrankensteuerung/include/espnow.h
+++ Schrankensteuerung/include/espnow.h
@@ -21,14 +21,18 @@ const uint8_t LED_BUILTIN = GPIO_NUM_18;
 // Für den SLAVE Mode muss der Decoder zunächst im WIFI_AP betrieben werden
 // Die spezifische MAC-Adresse erhält man mit  WiFi.softAPmacAddress()
 
-// die CntChannels sind die vier Gleisbesetztmelder, die die 
+// die CntChannels sind die vier Gleisbesetztmelder, die die
 // Schranke zum Öffnen oder Schließen bringen können
 const uint8_t maxCntChannels = 4;
 uint8_t channels[maxCntChannels];
 const uint8_t firstRMAdr = 1;
 const uint8_t lastRMAdr = 99;
 
-enum statustype {even = 0, odd};
+enum statustype
+{
+  even = 0,
+  odd
+};
 statustype currStatus;
 
 // die masterCustomMac-Adresse wurde für alle willkürlich festgelegt
@@ -37,37 +41,43 @@ const uint8_t masterCustomMac[] = {0x30, 0xAE, 0xA4, 0x89, 0x92, 0x71};
 
 esp_now_peer_info_t master;
 const esp_now_peer_info_t *masterNode = &master;
-uint8_t opFrame[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t opFrame[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 bool got1CANmsg = false;
-byte cnt=0;
+byte cnt = 0;
 String ssid0 = "CNgrSLV";
 deviceparams params;
-uint8_t hasharr[] = { 0x00, 0x00 };
+uint8_t hasharr[] = {0x00, 0x00};
 
 Ticker tckr1;
-const float tckr1Time = 0.025; 
+const float tckr1Time = 0.025;
 
 uint16_t secs;
 
 //*********************************************************************************************************
 // die beiden nächsten Routinen sind für das Blinken des Decoders zuständig
-void timer1s() {
-  if (secs>0) {
-  	if (secs % 2 == 0) {
+void timer1s()
+{
+  if (secs > 0)
+  {
+    if (secs % 2 == 0)
+    {
       digitalWrite(LED_BUILTIN, LOW);
     }
-    else {
+    else
+    {
       digitalWrite(LED_BUILTIN, HIGH);
     }
     secs--;
   }
-  else {
+  else
+  {
     digitalWrite(LED_BUILTIN, LOW);
   }
 }
 
-void stillAliveBlinkSetup() {
-  tckr1.attach(tckr1Time, timer1s);  // each sec
+void stillAliveBlinkSetup()
+{
+  tckr1.attach(tckr1Time, timer1s); // each sec
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   // turn the LED off by making the voltage LOW
@@ -87,12 +97,13 @@ Dieser 16 Bit Wert wird gebildet aus der UID Hash.
 Berechnung: 16 Bit High UID XOR 16 Bit Low der UID. Danach
 werden die Bits entsprechend zur CS1 Unterscheidung gesetzt.
 */
-void generateHash(uint8_t offset) {  
+void generateHash(uint8_t offset)
+{
   uint32_t uid = UID_BASE + offset;
-  params.uid_device[0] = (uint8_t) (uid >> 24);
-  params.uid_device[1] = (uint8_t) (uid >> 16);
-  params.uid_device[2] = (uint8_t) (uid >> 8);
-  params.uid_device[3] = (uint8_t) uid;
+  params.uid_device[0] = (uint8_t)(uid >> 24);
+  params.uid_device[1] = (uint8_t)(uid >> 16);
+  params.uid_device[2] = (uint8_t)(uid >> 8);
+  params.uid_device[3] = (uint8_t)uid;
 
   uint16_t highbyte = uid >> 16;
   uint16_t lowbyte = uid;
@@ -100,53 +111,69 @@ void generateHash(uint8_t offset) {
   bitWrite(hash, 7, 0);
   bitWrite(hash, 8, 1);
   bitWrite(hash, 9, 1);
-  hasharr[0] = hash>>8;
+  hasharr[0] = hash >> 8;
   hasharr[1] = hash;
 }
 
-void printMac(uint8_t m[macLen]) {
-  for (int ii = 0; ii < macLen-1; ++ii ) {
+void printMac(uint8_t m[macLen])
+{
+  for (int ii = 0; ii < macLen - 1; ++ii)
+  {
     Serial.print(m[ii], HEX);
     Serial.print(":");
   }
-  Serial.print(m[macLen-1], HEX);
+  Serial.print(m[macLen - 1], HEX);
 }
 
 // der Decoder strahlt mit dem Präfix und seiner Mac-Adresse
-void startAPMode() {
+void startAPMode()
+{
   Serial.println();
   Serial.println("WIFI Connect AP Mode");
   Serial.println("--------------------");
-  WiFi.persistent(false);                 // Turn off persistent to fix flash crashing issue.   
-  WiFi.mode(WIFI_OFF);                    // https://github.com/esp8266/Arduino/issues/3100 
-  WiFi.mode(WIFI_AP); 
-  
+  WiFi.persistent(false); // Turn off persistent to fix flash crashing issue.
+  WiFi.mode(WIFI_OFF);    // https://github.com/esp8266/Arduino/issues/3100
+  WiFi.mode(WIFI_AP);
+
   // Connect to Wi-Fi
   String ssid1 = WiFi.softAPmacAddress();
   ssid0 = ssid0 + ssid1;
   char ssid[30];
   ssid0.toCharArray(ssid, 30);
-  WiFi.softAP(ssid);  // Name des Access Points
+  WiFi.softAP(ssid); // Name des Access Points
   Serial.println(ssid);
 }
 
 // Falls mal etwas schiefgeht
-void printESPNowError(esp_err_t Result) {
-  if (Result == ESP_ERR_ESPNOW_NOT_INIT) {
+void printESPNowError(esp_err_t Result)
+{
+  if (Result == ESP_ERR_ESPNOW_NOT_INIT)
+  {
     // How did we get so far!!
     Serial.println("ESPNOW not Init.");
-  } else if (Result == ESP_ERR_ESPNOW_ARG) {
+  }
+  else if (Result == ESP_ERR_ESPNOW_ARG)
+  {
     Serial.println("Invalid Argument");
-  } else if (Result == ESP_ERR_ESPNOW_INTERNAL) {
+  }
+  else if (Result == ESP_ERR_ESPNOW_INTERNAL)
+  {
     Serial.println("Internal Error");
-  } else if (Result == ESP_ERR_ESPNOW_NO_MEM) {
+  }
+  else if (Result == ESP_ERR_ESPNOW_NO_MEM)
+  {
     Serial.println("ESP_ERR_ESPNOW_NO_MEM");
-  } else if (Result == ESP_ERR_ESPNOW_NOT_FOUND) {
+  }
+  else if (Result == ESP_ERR_ESPNOW_NOT_FOUND)
+  {
     Serial.println("Peer not found.");
-  } 
-    else if (Result == ESP_ERR_ESPNOW_IF) {
+  }
+  else if (Result == ESP_ERR_ESPNOW_IF)
+  {
     Serial.println("Interface Error.");
-  }   else {
+  }
+  else
+  {
     Serial.printf("\r\nNot sure what happened\t%d", Result);
   }
 }
@@ -155,100 +182,119 @@ void printESPNowError(esp_err_t Result) {
 // vom Master über ESPNow. Hier wird auch eine Erstauswertung
 // der Nachricht vorgenommen und dem Hauptprogramm mit der Variablen
 // got1CANmsg und einem Flag mitgeteilt, was weiterhein zu tun ist.
-void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
-  memcpy( opFrame, data, data_len );
-  if (data_len == macLen + 1) {
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len)
+{
+  memcpy(opFrame, data, data_len);
+  if (data_len == macLen + 1)
+  {
     // nur beim handshaking; es werden die macaddress
     // und die devicenummer übermittelt und die macaddress zurückgeschickt
     esp_err_t sendResult = esp_now_send(master.peer_addr, opFrame, macLen);
-    if (sendResult != ESP_OK) {
+    if (sendResult != ESP_OK)
+    {
       printESPNowError(sendResult);
     }
     generateHash(opFrame[macLen]);
     return;
   }
-  switch (opFrame[0x01]){
-    case BlinkAlive:
-    if (secs<10)
-        secs = 10;
+  switch (opFrame[0x01])
+  {
+  case BlinkAlive:
+    if (secs < 10)
+      secs = 10;
     break;
-    case PING: {
-      statusPING = true;
-      got1CANmsg = true;
+  case PING:
+  {
+    statusPING = true;
+    got1CANmsg = true;
+    // alles Weitere wird in loop erledigt
+  }
+  break;
+  // config
+  case CONFIG_Status:
+  {
+    CONFIG_Status_Request = true;
+    CONFIGURATION_Status_Index = (Kanals)opFrame[9];
+    if (CONFIGURATION_Status_Index > 0 && secs < 100)
+      secs = 100;
+    got1CANmsg = true;
+  }
+  break;
+  case SYS_CMD:
+  {
+    if (opFrame[9] == SYS_STAT)
+    {
+      SYS_CMD_Request = true;
       // alles Weitere wird in loop erledigt
-    }
-    break;
-    // config
-    case CONFIG_Status: {
-        CONFIG_Status_Request = true;
-        CONFIGURATION_Status_Index = (Kanals) opFrame[9];
-        if (CONFIGURATION_Status_Index>0 && secs<100)
-          secs = 100;
-        got1CANmsg = true;
-    }
-    break;
-    case SYS_CMD: {
-      if (opFrame[9]==SYS_STAT) {
-        SYS_CMD_Request = true;
-        // alles Weitere wird in loop erledigt
       got1CANmsg = true;
+    }
+  }
+  break;
+  case SWITCH_ACC:
+  {
+    // Umsetzung nur bei gültiger Weichenadresse
+    uint16_t _gate_address = (uint16_t)((opFrame[7] << 8) | opFrame[8]);
+    // berechnet die _gate_address aus der Decoderadresse und der Protokollkonstante
+    uint16_t _own_address = PROT + (params.decoderadr - 1) * 4;
+    // Auf benutzte Adresse überprüfen
+    if ((_own_address <= _gate_address) && ((_own_address + 4) >= _gate_address))
+    {
+      switchGate(_gate_address - _own_address);
+    }
+  }
+  break;
+  case S88_EVENT_R:
+  {
+    for (uint8_t ch = 0; ch < maxCntChannels; ch++)
+    {
+      if (channels[ch] == opFrame[0x08])
+      {
+        if (opFrame[0x0A] == 0x01)
+          switchGate(ch);
+        break;
       }
     }
-    break;
-    case SWITCH_ACC: {
-      // Umsetzung nur bei gültiger Weichenadresse
-      uint16_t _gate_address = (uint16_t) ((opFrame[7] << 8) | opFrame[8]);
-      // berechnet die _gate_address aus der Decoderadresse und der Protokollkonstante
-      uint16_t _own_address = PROT + (params.decoderadr - 1) * 4;
-      // Auf benutzte Adresse überprüfen
-      if ((_own_address <= _gate_address) && ((_own_address + 4) >= _gate_address)) {
-        switchGate(_gate_address-_own_address);
-      }
-    }
-    break;
-    case S88_EVENT_R: {
-      for (uint8_t ch = 0; ch < maxCntChannels; ch++) {
-        if (channels[ch] == opFrame[0x08]) {
-          if (opFrame[0x0A] == 0x01)
-            switchGate(ch);
-          break;
-        }
-      }
-    }
-    break;
+  }
+  break;
   }
 }
 
 // die Senderoutine
-void sendTheData() {
-  esp_now_send(master.peer_addr, opFrame, CAN_ENCAP_SIZE);
+void sendTheData()
+{
+  esp_now_send(master.peer_addr, opFrame, CAN_FRAME_SIZE);
 }
 
 // Auswertung, ob die Sendung erfolgreich war
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "OK" : "Fail");
 }
 
-// Hiermit wird der ESPNow initialisert, die Sende - und Empfangsroutine registiert 
+// Hiermit wird der ESPNow initialisert, die Sende - und Empfangsroutine registiert
 // sowieder Master angemeldet
-void addMaster() {
-  if(esp_now_init() == ESP_OK) {
+void addMaster()
+{
+  if (esp_now_init() == ESP_OK)
+  {
     Serial.println("ESPNow Init Success!");
   }
-  else {
+  else
+  {
     Serial.println("ESPNow Init Failed....");
   }
   esp_now_register_recv_cb(OnDataRecv);
   esp_now_register_send_cb(OnDataSent);
-	memcpy( &master.peer_addr, &masterCustomMac, macLen );
+  memcpy(&master.peer_addr, &masterCustomMac, macLen);
   master.channel = WIFI_CHANNEL; // pick a channel
-  master.encrypt = 0; // no encryption
+  master.encrypt = 0;            // no encryption
   //Add the master node to this slave node
   master.ifidx = ESP_IF_WIFI_AP;
   //Add the remote master node to this slave node
-	if(esp_now_add_peer(masterNode) == ESP_OK) {
-		Serial.println("Master Added As Peer!");
-	}
+  if (esp_now_add_peer(masterNode) == ESP_OK)
+  {
+    Serial.println("Master Added As Peer!");
+  }
 }
 
 #endif
